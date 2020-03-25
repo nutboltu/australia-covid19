6cdde9b394abb34f700232019991d2977f32dc17
farhadyasir <iraz2426@gmail.com>
2020-03-25 22:22:37 +1100

    feat: add nsw to date data

--- download-data/index.js
+++ download-data/index.js
@@ -1,11 +1,13 @@
 const downloadTimeSeries = require('./download-time-series-report');
 const updateAustraliaStateData = require('./update-australia-state-data');
 const downloadDailyReport = require('./download-daily-report');
+const getNSWData = require('./get-nsw-data');
 
 const downloadAllData = async () => {
   await downloadTimeSeries();
   await downloadDailyReport();
   updateAustraliaStateData();
+  getNSWData();
 }
 
 module.exports = downloadAllData();
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head';
 import { FilteredGraph } from '../components/filtered-graph';
+import { BarGraph } from '../components/bar-graph';
+
 import confirmed from "../data/australia/confirmed.json";
 import deaths from "../data/australia/deaths.json";
 import recovered from "../data/australia/recovered.json";
+import nswSexAndAgeGroup from "../data/australia/nsw/sex-age-group.json";
 
-const Home = ({ confirmed, deaths, recovered }) => {
+const Home = ({ confirmed, deaths, recovered, nswSexAndAgeGroup }) => {
   return (
   <div className="container">
     <Head>
@@ -13,10 +16,14 @@ const Home = ({ confirmed, deaths, recovered }) => {
     </Head>
 
     <main>
-      <div style={{ height: '400px', width: '900px'}}>
+      <div style={{ height: '600px', width: '900px'}}>
+        <h2>Overall Australia - cases to date</h2>
         <FilteredGraph data={confirmed} title="Confirmed" />
         <FilteredGraph data={deaths} title="Deaths" />
         <FilteredGraph data={recovered} title="Recovered" />
+        <h2>NSW - Confirmed cases to date</h2>
+        <h3>By sex and age group</h3>
+        <BarGraph data={nswSexAndAgeGroup} />
       </div>
     </main>
 
@@ -33,6 +40,7 @@ export async function getStaticProps() {
       confirmed,
       deaths,
       recovered,
+      nswSexAndAgeGroup,
     },
   }
 }
