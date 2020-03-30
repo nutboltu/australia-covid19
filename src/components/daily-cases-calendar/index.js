e65756f6b5b09fa267e221c033b895d76d730ee2
farhadyasir <iraz2426@gmail.com>
2020-03-31 00:49:12 +1100

    feat: new Daily calendar

---
+++ src/components/daily-cases-calendar/index.js
@@ -0,0 +1,37 @@
+import { Divider } from 'antd';
+import { ResponsiveCalendar } from '@nivo/calendar';
+import daily from '../../data/daily-reports.json';
+
+export const DailyCasesCalendar = ({ stateName }) => {
+    return (
+        <>
+            <Divider orientation="center">
+                Daily confirmed cases
+            </Divider>
+            <ResponsiveCalendar
+                data={daily[stateName]}
+            from="2020-02-01"
+            to="2020-12-31"
+            emptyColor="#eeeeee"
+            colors={[ '#fcbba1', '#fc9297', '#fb614a', '#ef3b2c', '#cb181d', '#67000d' ]}
+            margin={{ top: -40, right: 40, bottom: 40, left: 40 }}
+            yearSpacing={0}
+            monthBorderColor="#ffffff"
+            dayBorderWidth={2}
+            dayBorderColor="#ffffff"
+            legends={[
+                {
+                    anchor: 'bottom-right',
+                    direction: 'row',
+                    translateY: -32,
+                    itemCount: 4,
+                    itemWidth: 42,
+                    itemHeight: 36,
+                    itemsSpacing: 14,
+                    itemDirection: 'right-to-left'
+                }
+            ]}
+        />
+        </>
+    );
+}
