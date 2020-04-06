ff3cd5e75e2477b5524d8b9dea4f9a575ef02462
nutboltu <farhad.yasir.nb@gmail.com>
2020-04-06 20:34:29 +1000

    fix: nsw crawler

--- src/data-fetch/fetch-nsw-data.js
+++ src/data-fetch/fetch-nsw-data.js
@@ -11,8 +11,10 @@ const sources = [
 ]
 const fetchNSWData = async () => {
   let response;
+  let responseLD;
   try {
     response = await axios.get("https://www.health.nsw.gov.au/Infectious/diseases/Pages/covid-19-latest.aspx#statistics");
+    responseLD = await axios.get("https://www.health.nsw.gov.au/Infectious/diseases/Pages/covid-19-lhd.aspx");
     if (response.status !== 200) {
       console.log("ERROR");
     }
@@ -20,60 +22,60 @@ const fetchNSWData = async () => {
     return null;
   }
   const html = cheerio.load(response.data);
+  const ldHtml = cheerio.load(responseLD.data);
   let nswCases = {};
   let nswTested = [];
   let nswConfirmedCases = [];
   let sourcesOfInfection = [];
   let sexAndAgeGroup = [];
   let localDistrictCases = [];
-
   html(".moh-rteTable-6")
     .filter((i, el) => {
-      const tbody = el.children[0];
-    // if ( i != 3) {
-    //   return;
-    // }
     if ( i == 0 ) {
-      const totalConfirmed = toNumber(tbody.children[2].children[2].children[0].data);
-      const totalTested= toNumber(tbody.children[6].children[2].children[0].data);
-      nswTested = getTestedFormat(totalConfirmed, totalTested);
-      nswCases['confirmed'] = totalConfirmed;
+      const confirmed = toNumber(el.children[1].children[2].children[2].children[0].data);
+      const totalNegative= toNumber(el.children[1].children[8].children[3].children[0].data);
+      const deaths = toNumber(el.children[1].children[10].children[2].children[0].data);
+      nswTested = getTestedFormat(confirmed, totalNegative + confirmed);
+      nswCases = {
+        confirmed,
+        deaths,
+      }
     }
     if ( i == 1 ) {
-      nswCases['deaths'] = toNumber(tbody.children[8].children[2].children[0].data);
-    }
-    if ( i == 2 ) {
-      sexAndAgeGroup = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20].reduce((acc, index) => {
+      sexAndAgeGroup = [3,5,7,9,11,13,15,17,19,21].reduce((acc, index) => {
         const item = {
-          age: tbody.children[index].children[1].children[0].data,
-          female: toNumber(tbody.children[index].children[3].children[0].data),
-          male: toNumber(tbody.children[index].children[5].children[0].data),
-          total: toNumber(tbody.children[index].children[7].children[0].data),
+          age: el.children[1].children[index].children[1].children[0].data,
+          female: toNumber(el.children[1].children[index].children[3].children[0].data),
+          male: toNumber(el.children[1].children[index].children[5].children[0].data),
         }
         acc.push(item);
         return acc;
       }, [])
     }
-    if ( i == 3) {
+    if ( i == 2) {
       sourcesOfInfection = [3,5,7,9].reduce((acc, index, i) => {
         const value = toNumber(el.children[1].children[index].children[3].children[0].data);
         acc.push({ id: sources[i], label: sources[i], value});
         return acc;
       }, [])
     }
-    if ( i == 4 ) {
-      localDistrictCases = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30].reduce((acc, index) => {
-        const item = {
-          district: el.children[0].children[index].children[1].children[0].data.replace('\n ', ''),
-          cases: toNumber(el.children[0].children[index].children[3].children[0].data),
-          test: toNumber(el.children[0].children[index].children[5].children[0].data),
-          positive_percentage: el.children[0].children[index].children[7].children[0].data,
-        }
-        acc.push(item);
-        return acc;
-      }, []);
-    }
   });
+
+  ldHtml(".moh-rteTable-6")
+  .filter((i, el) => {
+      if (i == 0) {
+          localDistrictCases = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30].reduce((acc, index) => {
+            const item = {
+              district: el.children[0].children[index].children[1].children[0].data.replace('\n ', ''),
+              cases: toNumber(el.children[0].children[index].children[3].children[0].data),
+              test: toNumber(el.children[0].children[index].children[5].children[0].data),
+              positive_percentage: el.children[0].children[index].children[7].children[0].data,
+            }
+            acc.push(item);
+            return acc;
+          }, []);
+      }
+  })
 
   // console.log(nswCases, nswTested, nswConfirmedCases, sourcesOfInfection, sexAndAgeGroup, localDistrictCases)
   write('./src/data/nsw/cases.json', JSON.stringify(nswCases));
